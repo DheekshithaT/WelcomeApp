a6fbaa540f92575c1141441534484044d3a51327
dixie354 <dheekshithaduri2@gmail.com>
2024-02-15 12:08:04 +0530

    Final Commit

---
+++ SignIn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+	public class SignIn
+	{
+		public string Authentication(string userName, string password)
+		{
+			string msg;
+			if ((string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password)))
+			{
+				msg = "Username and password required";
+			}
+			else
+			{
+				if ((userName == "admin") && (password == "admin@1256"))
+				{
+					msg = "Authentication Pass"
+				}
+				else
+				{
+					msg = "Authentication Fail";
+				}
+			}
+			return msg;
+		}
+	}
+}
